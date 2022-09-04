90510d567c8c1e0a4f66331cbf60d6368029f9df
MithunKumarSharma <39516519+MithunKumarSharma@users.noreply.github.com>
2022-09-04 11:16:54 +0100

    DialogExample

--- src/test/java/mithunsharma/DialogGestureExample.java
+++ src/test/java/mithunsharma/DialogGestureExample.java
@@ -1,5 +1,17 @@
 package mithunsharma;
 
-public class DialogGestureExample {
-
+public class DialogGestureExample extends BaseTest{
+@Test
+	public void validateDialog() throws InterruptedException {
+		driver.findElement(AppiumBy.accessibilityId("App")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Alert Dialogs\"]")).click();
+		driver.findElement(AppiumBy.accessibilityId("OK Cancel dialog with a message")).click();
+		Thread.sleep(1000);
+		boolean isDisplayed = driver.findElement(By.id("android:id/alertTitle")).isDisplayed();
+		if(isDisplayed) {
+		   driver.findElement(By.id("android:id/button1")).click();
+		} else {
+			driver.findElement(By.id("android:id/button2")).click();
+		}
+	}
 }
