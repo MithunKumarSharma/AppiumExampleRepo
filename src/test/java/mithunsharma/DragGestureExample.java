6b42b74eb604dd9bf2c44e59b5bcbb1ad5835208
MithunKumarSharma <39516519+MithunKumarSharma@users.noreply.github.com>
2022-09-04 11:53:13 +0100

    Add files via upload

---
+++ src/test/java/mithunsharma/DragGestureExample.java
@@ -0,0 +1,25 @@
+package mithunsharma;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+
+public class DragGestureExample extends BaseTest{
+	@Test
+	public void DragExample() throws InterruptedException {
+		String direction= "down";
+		scrollAction(direction);
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+		driver .findElement(By.xpath("//android.widget.TextView[@content-desc=\"Drag and Drop\"]")).click();
+		WebElement dragCircle = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+		dragGestureAction(dragCircle, 472, 425);
+		String result = driver.findElement(By.id("io.appium.android.apis:id/drag_result_text")).getText();
+		Assert.assertEquals(result, "Dropped!");
+		dragGestureAction(dragCircle, 158, 742);
+		Assert.assertEquals(result, "Dropped!");
+		
+	}
+}
