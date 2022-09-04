f2d04cc239575fbd282696db5417232bff1cf96d
MithunKumarSharma <39516519+MithunKumarSharma@users.noreply.github.com>
2022-09-04 11:52:13 +0100

    Add files via upload

---
+++ src/test/java/mithunsharma/DialogExample.java
@@ -0,0 +1,22 @@
+package mithunsharma;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+
+public class DialogExample extends BaseTest{
+	@Test
+	public void validateDialog() throws InterruptedException {
+		driver.findElement(AppiumBy.accessibilityId("App")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Alert Dialogs\"]")).click();
+		driver.findElement(AppiumBy.accessibilityId("OK Cancel dialog with a message")).click();
+		Thread.sleep(1000);
+		boolean isDisplayed = driver.findElement(By.id("android:id/alertTitle")).isDisplayed();
+		if(isDisplayed) {
+		   driver.findElement(By.id("android:id/button1")).click();
+		} else {
+			driver.findElement(By.id("android:id/button2")).click();
+		}
+	}
+}
